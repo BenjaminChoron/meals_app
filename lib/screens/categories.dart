a7981bbb758399d6d4c886db14ea091c0b18d492
BenjCH <benjamin.choron@outlook.com>
2024-07-09 16:32:46 +0200

    💫 Explicite animation on categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -25,12 +25,15 @@ class _CategoriesScreenState extends State<CategoriesScreen>
   @override
   void initState() {
     super.initState();
+
     _animationController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 300),
       lowerBound: 0,
       upperBound: 1,
     );
+
+    _animationController.forward();
   }
 
   @override
@@ -56,20 +59,35 @@ class _CategoriesScreenState extends State<CategoriesScreen>
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: availableCategories.map((category) {
+          return CategoryGridItem(
+            category: category,
+            onSelectCategory: () => _selectCategory(context, category),
+          );
+        }).toList(),
       ),
-      children: availableCategories.map((category) {
-        return CategoryGridItem(
-          category: category,
-          onSelectCategory: () => _selectCategory(context, category),
-        );
-      }).toList(),
+      builder: (context, child) => SlideTransition(
+        position: Tween<Offset>(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
+        ),
+        child: child,
+      ),
     );
   }
 }
